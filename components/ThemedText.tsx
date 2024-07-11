4a82755e8175521e8f516421c76a3584ed2c6dcc
Kayton <kaytoncwc@gmail.com>
2024-07-12 03:43:12 +0800

    theme: secondary text

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -5,22 +5,24 @@ import { useThemeColor } from "@/hooks/useThemeColor";
 export type ThemedTextProps = TextProps & {
     lightColor?: string;
     darkColor?: string;
-    type?: "default" | "title" | "defaultSemiBold" | "subtitle" | "link" | "error";
+    type?: "default" | "title" | "defaultSemiBold" | "subtitle" | "link" | "error" | "secondary";
 };
 
 export function ThemedText({ style, lightColor, darkColor, type = "default", ...rest }: ThemedTextProps) {
     const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+    const secondaryColor = useThemeColor({ light: lightColor, dark: darkColor }, "textSecondary");
 
     return (
         <Text
             style={[
-                { color },
+                { color: type === "secondary" ? secondaryColor : color },
                 type === "default" ? styles.default : undefined,
                 type === "title" ? styles.title : undefined,
                 type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
                 type === "subtitle" ? styles.subtitle : undefined,
                 type === "link" ? styles.link : undefined,
                 type === "error" ? styles.error : undefined,
+                type === "secondary" ? styles.secondary : undefined,
                 style,
             ]}
             {...rest}
@@ -56,5 +58,8 @@ const styles = StyleSheet.create({
         fontSize: 16,
         lineHeight: 24,
         color: "red",
+    },
+    secondary: {
+        fontSize: 14,
     },
 });
